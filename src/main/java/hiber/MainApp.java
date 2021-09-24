c7c55f17db6ce3709a4759c8ba34e26cb2f7fd06
prod.god <prod.god666@gmail.com>
2021-09-24 14:08:28 +0300

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,48 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.persistence.NoResultException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Toyota Corolla", 197)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Tesla Model X", 965)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Ford Bronco", 5312)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Fiat Panda", 3321)));
+
+
+         List<User> usersByCar = userService.userByCarId("Tesla Model X", 965);
+         for (User user : usersByCar) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car= " + user.getCar());
+         }
+
+
+//      List<User> users = userService.listUsers();
+//      for (User user : users) {
+//         System.out.println("Id = "+user.getId());
+//         System.out.println("First Name = "+user.getFirstName());
+//         System.out.println("Last Name = "+user.getLastName());
+//         System.out.println("Email = "+user.getEmail());
+//         System.out.println("Car= " + user.getCar());
+//         System.out.println();
+//      }
+
+      context.close();
+   }
+}
